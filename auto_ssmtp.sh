2c5a097725cc7479940e162331fee54487f3d354
Panagiotis Papaioannou <65160692+papajohn-uop@users.noreply.github.com>
2020-07-06 15:21:08 +0300

    Update auto_ssmtp.sh

--- auto_ssmtp.sh
+++ auto_ssmtp.sh
@@ -1,1 +1,7 @@
 
+echo UseSTARTTLS=YES >> /etc/ssmtp/ssmtp.conf
+echo FromLineOverride=YES >> /etc/ssmtp/ssmtp.conf
+echo root=ego@esy.com >> /etc/ssmtp/ssmtp.conf
+echo mailhub=smtp.gmail.com:587 >> /etc/ssmtp/ssmtp.conf
+echo authuser=email >> /etc/ssmtp/ssmtp.conf
+echo AuthPass=pass >> /etc/ssmtp/ssmtp.conf
