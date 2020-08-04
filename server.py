11b41245fcba9fc49c8923cafad0fb2e9c155cd6
Panagiotis Papaioannou <65160692+papajohn-uop@users.noreply.github.com>
2020-08-04 16:22:05 +0300

    Update server.py

--- server.py
+++ server.py
@@ -19,13 +19,13 @@ class S(BaseHTTPRequestHandler):
         logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
         self._set_response()
         #self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
-        logging.info("PAPAJOHN:%s",self.mylist)
+        #logging.info("PAPAJOHN:%s",self.mylist)
         self.wfile.write("GET request for {}{}".format(self.path, self.mylist).encode('utf-8'))
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
         post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+        #logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
                 str(self.path), str(self.headers), post_data.decode('utf-8'))
 
         self._set_response()
