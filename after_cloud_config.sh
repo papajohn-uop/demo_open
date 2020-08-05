805eedfa658ca16d5cedd2274cd707f7e3c022f8
Panagiotis Papaioannou <65160692+papajohn-uop@users.noreply.github.com>
2020-08-05 15:27:26 +0300

    Update after_cloud_config.sh

--- after_cloud_config.sh
+++ after_cloud_config.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
-#echo "Start of test script for after cloud init has finished"
 set -e
 # Block until cloud-init completes
-#cloud-init status --wait  > /dev/null 2>&1 [ $? -ne 0 ] && echo 'Cloud-init failed' && exit 1
 cloud-init status --wait  > /dev/null 2>&1
 [ $? -ne 0 ] && echo 'Cloud-init failed' && exit 1
 echo 'Cloud-init succeeded at ' `date -R`  > /home/ubuntu/post-cloud-init.log
 
+
+
+#echo "Start of test script for after cloud init has finished"
+##set -e
+# Block until cloud-init completes
+#cloud-init status --wait  > /dev/null 2>&1 [ $? -ne 0 ] && echo 'Cloud-init failed' && exit 1
+##cloud-init status --wait  > /dev/null 2>&1
+##[ $? -ne 0 ] && echo 'Cloud-init failed' && exit 1
+##echo 'Cloud-init succeeded at ' `date -R`  > /home/ubuntu/post-cloud-init.log
+
 #echo 'Cloud-init succeeded at ' `date -R`  
-echo 'Cloud-init succeeded at ' `date -R`  > /home/ubuntu/post-cloud-init.log
+##echo 'Cloud-init succeeded at ' `date -R`  > /home/ubuntu/post-cloud-init.log
 #sudo sh -c 'echo "Cloud-init succeeded at " `date -R` >>/file.txt'
 #echo "End of test script for after cloud init has finished"
 #echo "First arg: $1"
 # Make your magic happen here
-tail -5 /var/log/cloud-init-output.log >> /home/ubuntu/post-cloud-init.log
+##tail -5 /var/log/cloud-init-output.log >> /home/ubuntu/post-cloud-init.log
 #tail -1 /var/log/cloud-init-output.log  |awk '{print $(NF -1), $NF}' > /home/ubuntu/time.txt
 #curl -d "@/home/ubuntu/time.txt" $1
 
