68dc74deda30926fa60289e467a7dc82fd9ea8bc
Panagiotis Papaioannou <65160692+papajohn-uop@users.noreply.github.com>
2020-08-05 13:58:58 +0300

    Update after_cloud_config.sh

--- after_cloud_config.sh
+++ after_cloud_config.sh
@@ -5,6 +5,7 @@ set -e
 cloud-init status --wait  > /dev/null 2>&1
 [ $? -ne 0 ] && echo 'Cloud-init failed' && exit 1
 echo 'Cloud-init succeeded at ' `date -R`  
-echo 'Cloud-init succeeded at ' `date -R`  > /home/ubuntu/post-cloud-init.log
+#echo 'Cloud-init succeeded at ' `date -R`  > /home/ubuntu/post-cloud-init.log
+sudo sh -c 'echo "Cloud-init succeeded at " `date -R` >>/file.txt'
 echo "End of test script for after cloud init has finished"
 # Make your magic happen here
