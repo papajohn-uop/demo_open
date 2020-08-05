9ddf698d234ce5a473194dcb1a180e9e7a6cefb0
Panagiotis Papaioannou <65160692+papajohn-uop@users.noreply.github.com>
2020-08-05 16:01:55 +0300

    Update after_cloud_config.sh

--- after_cloud_config.sh
+++ after_cloud_config.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 # Block until cloud-init completes
-#cloud-init status --wait  > /dev/null 2>&1
-#[ $? -ne 0 ] && echo 'Cloud-init failed' && exit 1
-#echo 'Cloud-init succeeded at ' `date -R`  > /home/ubuntu/post-cloud-init.log
+cloud-init status --wait  > /dev/null 2>&1
+[ $? -ne 0 ] && echo 'Cloud-init failed' && exit 1
+echo 'Cloud-init succeeded at ' `date -R`  > /home/ubuntu/post-cloud-init.log
 echo "First arg: $1"
 tail -5 /var/log/cloud-init-output.log >> /home/ubuntu/post-cloud-init.log
 
