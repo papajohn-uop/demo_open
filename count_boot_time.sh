4aed4ffaeb5d50c974aaaf5e7ea9fc082abd0e24
Panagiotis Papaioannou <65160692+papajohn-uop@users.noreply.github.com>
2020-08-13 09:46:04 +0300

    Update count_boot_time.sh

--- count_boot_time.sh
+++ count_boot_time.sh
@@ -1,14 +1,15 @@
+POST request for /ubuntu@auto-vpn-time-1-vpnservervnf-vm-1:~$ cat count_boot_time.sh
 echo {  > /home/ubuntu/time.txt
-echo -n "\"HOSTNAME\": " >> /home/ubuntu/time.txt
-echo "\" $(hostname) \", " >> /home/ubuntu/time.txt
+echo -n "{\"HOSTNAME\": " >> /home/ubuntu/time.txt
+echo -n "\" $(hostname) \", " >> /home/ubuntu/time.txt
 echo -n "\"PUBLIC IP\": " >> /home/ubuntu/time.txt
-echo "\" $(ec2metadata --public-ip) \", " >> /home/ubuntu/time.txt
+echo -n "\" $(ec2metadata --public-ip) \", " >> /home/ubuntu/time.txt
 echo -n "\"LOCAL IP\": " >> /home/ubuntu/time.txt
-echo "\" $(ec2metadata --local-ip) \", " >> /home/ubuntu/time.txt
+echo -n "\" $(ec2metadata --local-ip) \", " >> /home/ubuntu/time.txt
 echo -n "\"FIRST BOOT\": " >> /home/ubuntu/time.txt
-echo  -n "$(cat /var/log/cloud-init-output.log |grep DataSourceOpenStackLocal |head -1 | awk '{print $(NF -1), $NF}')" >> /home/ubuntu/time.txt
-echo  ,  >> /home/ubuntu/time.txt
+echo -n "$(cat /var/log/cloud-init-output.log |grep DataSourceOpenStackLocal |head -1 | awk '{print $(NF -1), $NF}') ," >> /home/ubuntu/time.txt
 echo -n "\"LAST BOOT\": " >> /home/ubuntu/time.txt
-echo  "$(cat /var/log/cloud-init-output.log |grep DataSourceOpenStackLocal |tail -1 | awk '{print $(NF -1), $NF}')" >> /home/ubuntu/time.txt
-echo } >> /home/ubuntu/time.txt
+echo -n "$(cat /var/log/cloud-init-output.log |grep DataSourceOpenStackLocal |tail -1 | awk '{print $(NF -1), $NF}')" >> /home/ubuntu/time.txt
+echo -n } >> /home/ubuntu/time.txt
 curl --data-binary "@/home/ubuntu/time.txt" http://150.140.195.241:9090
+
