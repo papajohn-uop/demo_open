a4cff30be677323f66f1799dde6bb9ea623d6284
Panagiotis Papaioannou <65160692+papajohn-uop@users.noreply.github.com>
2020-08-14 15:29:25 +0300

    Update count_boot_time.sh

--- count_boot_time.sh
+++ count_boot_time.sh
@@ -6,9 +6,10 @@ echo -n "\" $(ec2metadata --public-ip) \", " >> /home/ubuntu/time.txt
 echo -n "\"LOCAL IP\": " >> /home/ubuntu/time.txt
 echo -n "\" $(ec2metadata --local-ip) \", " >> /home/ubuntu/time.txt
 echo -n "\"FIRST BOOT\": " >> /home/ubuntu/time.txt
-echo -n "$(cat /var/log/cloud-init-output.log |grep DataSourceOpenStackLocal |head -1 | awk '{print $(NF -1), $NF}') ," >> /home/ubuntu/time.txt
-echo -n "\"LAST BOOT\": " >> /home/ubuntu/time.txt
-echo -n "$(cat /var/log/cloud-init-output.log |grep DataSourceOpenStackLocal |tail -1 | awk '{print $(NF -1), $NF}')" >> /home/ubuntu/time.txt
+echo -n "$(cloud-init analyze show |grep Total)" >> /home/ubuntu/time.txt
+#echo -n "$(cat /var/log/cloud-init-output.log |grep DataSourceOpenStackLocal |head -1 | awk '{print $(NF -1), $NF}') ," >> /home/ubuntu/time.txt
+#echo -n "\"LAST BOOT\": " >> /home/ubuntu/time.txt
+#echo -n "$(cat /var/log/cloud-init-output.log |grep DataSourceOpenStackLocal |tail -1 | awk '{print $(NF -1), $NF}')" >> /home/ubuntu/time.txt
 echo -n } >> /home/ubuntu/time.txt
 curl --data-binary "@/home/ubuntu/time.txt" http://150.140.195.241:9090
 
