a90887c426344a3a4ba7eeb1c3568283c94ca246
Panagiotis Papaioannou <65160692+papajohn-uop@users.noreply.github.com>
2020-10-07 12:05:07 +0300

    Update monitor_mme_poc.chart.py

--- monitor_mme_poc.chart.py
+++ monitor_mme_poc.chart.py
@@ -30,7 +30,7 @@ items2handle={
 
 
 class Service(ExecutableService):
-    
+
     def __init__(self, configuration=None, name=None):
         ExecutableService.__init__(self, configuration=configuration, name=name)
         #self.order = ORDER
@@ -43,27 +43,27 @@ class Service(ExecutableService):
         #data in json format
         self.json=None
         #json of stripped data to display
-    self.ues_json=None
+        self.ues_json=None
         #values to show at graphs
-    self.values=dict()
+        self.values=dict()
 
-    
-    
+
+
     #create json from data
     def createJson(self):
-        
+
         tmp_data=None
         data=None
-        self.debug("********************************")
+        self.debug("********************************1")
         self.debug(self.data)
-        self.debug("********************************")
+        self.debug("********************************2")
         tmp_data="".join(self.data)
         self.debug(tmp_data)
-        self.debug("********************************")
+        self.debug("********************************3")
 
-        self.debug(tmp_data[176])  
-        
-        
+        self.debug(tmp_data[176:])
+
+
         #data =tmp_data.read()[176:]  # json.load(json_file)
         data =json.loads(tmp_data[176:])  # json.load(json_file)
         self.json=data
@@ -79,7 +79,7 @@ class Service(ExecutableService):
                 if key  in  self.ues_json[index]:
                     del self.ues_json[index][key]
 
-    
+
     #create charts for mme/ue_get
     def mme_ue_get_charts(self):
         #case 1:
@@ -93,7 +93,7 @@ class Service(ExecutableService):
         :return: dict
         """
         raw_data = self._get_raw_data()
-        
+
         self.debug("*************************")
         self.debug(raw_data)
         if not raw_data:
@@ -111,19 +111,19 @@ class Service(ExecutableService):
 
         #strip initial json and get ues
         self.stripJson()
-       
-        #start creating charts
-        self.mme_ue_get_charts()        
 
-    
-   
+        #start creating charts
+        self.mme_ue_get_charts()
+
+
+
         try:
             # for non zero valuesretVal = {x:y for x,y in self.values.items() if y!=0}
             return self.values
             #return retVal
         except (ValueError, AttributeError):
             return None
-         
+
     def create_charts_incremental(self,aggregate, server_list):
         if aggregate:
             order = ['dns_group']
@@ -149,49 +149,48 @@ class Service(ExecutableService):
             #order = [''.join(['dns_', ns.replace('.', '_')]) for ns in server_list]
             ues_count=len(self.json['ue_list'])
             for index in range(ues_count):
-                tmp_ue=self.ues_json[index]      
-                #get data for each bearer 
+                tmp_ue=self.ues_json[index]
+                #get data for each bearer
                 tmp_bearer_count=0
                 if 'bearers' in tmp_ue:
-                    #get bearers
+                #get bearers
                     tmp_bearers=tmp_ue['bearers']
                     tmp_bearer_count=len(tmp_ue['bearers'])
-                
                     for bear_index in range(tmp_bearer_count):
                     #get each bearer
-                    tmp_bearer=tmp_bearers[bear_index]
-		    #tmp_order='_'.join(('INCR_ue',str(index),'bearer',str(bear_index),'__'))
-	            tmp_order='_'.join((tmp_ue['imsi'],tmp_bearer['ip']))
-		    ul_line= 'upload'.join(('ue',str(index),'bearer',str(bear_index),'ul'))
-		    dl_line= 'download'.join(('ue',str(index),'bearer',str(bear_index),'dl'))
-		    order.append(tmp_order)
-            definitions[tmp_order] = {
-                      'options': [None, 'Bytes', 'bytes', tmp_order, 'bytes', 'area'],
+                        tmp_bearer=tmp_bearers[bear_index]
+                        #tmp_order='_'.join(('INCR_ue',str(index),'bearer',str(bear_index),'__'))
+                        tmp_order='_'.join((tmp_ue['imsi'],tmp_bearer['ip']))
+                        ul_line= 'upload'.join(('ue',str(index),'bearer',str(bear_index),'ul'))
+                        dl_line= 'download'.join(('ue',str(index),'bearer',str(bear_index),'dl'))
+                        order.append(tmp_order)
+                        definitions[tmp_order] = {
+                            'options': [None, 'Bytes', 'bytes', tmp_order, 'bytes', 'area'],
                             'lines': [
                                 [
                                     ul_line,
                                     ul_line,
                                   #  'absolute',
-                                            'incremental'
+                                    'incremental'
                                 ],
-                        [
+                                [
                                     dl_line,
                                     dl_line,
                                   #  'absolute',
-                                            'incremental'
+                                    'incremental'
                                 ]
                             ]
                         }
-            #create a dict with the values for each line 
-            #TODO: Once this works create the this dict on another function and use as required
-            self.values[ul_line] = tmp_bearer['ul_total_bytes'] 
-            self.values[dl_line] = tmp_bearer['dl_total_bytes'] 
+                        #create a dict with the values for each line
+                        #TODO: Once this works create the this dict on another function and use as required
+                        self.values[ul_line] = tmp_bearer['ul_total_bytes']
+                        self.values[dl_line] = tmp_bearer['dl_total_bytes']
 
             self.order= order
             self.definitions=definitions
 
 
-    
+
 '''
     def create_charts_absolute(self,aggregate, server_list):
         if aggregate:
@@ -211,50 +210,47 @@ class Service(ExecutableService):
                 definitions['dns_group']['lines'].append(dim)
 
             self.order= order
-        self.definitions=definitions
+            self.definitions=definitions
         else:
             order=list()
             definitions = dict()
             #order = [''.join(['dns_', ns.replace('.', '_')]) for ns in server_list]
             ues_count=len(self.json['ue_list'])
-        for index in range(ues_count):
-        tmp_ue=self.ues_json[index]      
-        #get data for each bearer 
-        tmp_bearer_count=0
-        if 'bearers' in tmp_ue:
-            #get bearers
-            tmp_bearers=tmp_ue['bearers']
-            tmp_bearer_count=len(tmp_ue['bearers'])
-        
-            for bear_index in range(tmp_bearer_count):
-            #get each bearer
-                tmp_bearer=tmp_bearers[bear_index]
-                tmp_order='_'.join(('ue',str(index),'bearer',str(bear_index),'__'))
-                ul_line= '_'.join(('ue',str(index),'bearer',str(bear_index),'ul'))
-                dl_line= '_'.join(('ue',str(index),'bearer',str(bear_index),'dl'))
-                order.append(tmp_order)
-                definitions[tmp_order] = {
-                    'options': [None, 'DNS Response Time', 'ms', tmp_order, 'dns_query_time.response_time', 'area'],
-                    'lines': [
-                        [
-                            ul_line,
-                            ul_line,
-                            'absolute',
+                for index in range(ues_count):
+                        tmp_ue=self.ues_json[index]
+                        #get data for each bearer
+                        tmp_bearer_count=0
+                        if 'bearers' in tmp_ue:
+                       #get bearers
+                           tmp_bearers=tmp_ue['bearers']
+                           tmp_bearer_count=len(tmp_ue['bearers'])
+                       for bear_index in range(tmp_bearer_count):
+                               #get each bearer
+                                    tmp_bearer=tmp_bearers[bear_index]
+                        tmp_order='_'.join(('ue',str(index),'bearer',str(bear_index),'__'))
+                        ul_line= '_'.join(('ue',str(index),'bearer',str(bear_index),'ul'))
+                        dl_line= '_'.join(('ue',str(index),'bearer',str(bear_index),'dl'))
+                        order.append(tmp_order)
+                                definitions[tmp_order] = {
+                            'options': [None, 'DNS Response Time', 'ms', tmp_order, 'dns_query_time.response_time', 'area'],
+                            'lines': [
+                                [
+                                    ul_line,
+                                    ul_line,
+                                    'absolute',
                                 ],
-                [
-                            dl_line,
-                            dl_line,
-                            'absolute'
-                        ]
-                    ]
-                }
-            #create a dict with the values for each line 
-            #TODO: Once this works create the this dict on another function and use as required
-            self.values[ul_line] = tmp_bearer['ul_total_bytes'] 
-            self.values[dl_line] = tmp_bearer['dl_total_bytes'] 
+                                [
+                                    dl_line,
+                                    dl_line,
+                                    'absolute'
+                                ]
+                            ]
+                        }
+                        #create a dict with the values for each line
+                        #TODO: Once this works create the this dict on another function and use as required
+                        self.values[ul_line] = tmp_bearer['ul_total_bytes']
+                        self.values[dl_line] = tmp_bearer['dl_total_bytes']
 
-            self.order= order
+        self.order= order
         self.definitions=definitions
 '''
-
-
